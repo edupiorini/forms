88d7bd685f8a928b2866de3b9b39884528939e88
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-28 17:03:01 -0300

    pessoa fisica migration set

--- priv/repo/migrations/20210628095402_create_pessoa_fisica_table.exs
+++ priv/repo/migrations/20210628095402_create_pessoa_fisica_table.exs
@@ -2,6 +2,18 @@ defmodule Forms.Repo.Migrations.CreatePessoaFisicaTable do
   use Ecto.Migration
 
   def change do
+    create table(:pessoa_fisica) do
+      add :nome, :string
+      add :endereco, :string
+      add :cidade, :string
+      add :estado, :string
+      add :telefone, {:array, :string}
+      add :cpf, :string
+      add :email, :string
 
+      timestamps()
+    end
+
+    create unique_index(:pessoa_fisica, [:cpf])
   end
 end
