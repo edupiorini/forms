464ca60e1dd4600bd27e0e34f1a501fbd60ea37f
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-30 07:28:20 -0300

    facade applied

--- lib/forms.ex
+++ lib/forms.ex
@@ -1,4 +1,7 @@
 defmodule Forms do
+  alias Forms.Clientes.PessoaFisica.Create, as: CreatePessoaFisica
+  alias Forms.Clientes.PessoaJuridica.Create, as: CreatePessoaJuridica
+
   @moduledoc """
   Forms keeps the contexts that define your domain
   and business logic.
@@ -6,4 +9,7 @@ defmodule Forms do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  defdelegate create_pessoa_fisica(params), to: CreatePessoaFisica, as: :call
+  defdelegate create_pessoa_juridica(params), to: CreatePessoaJuridica, as: :call
 end
