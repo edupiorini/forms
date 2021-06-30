2acb4462189a67dbc3ceb6ce20ecd3182a327154
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-30 06:20:46 -0300

    responsabilities were separated

---
+++ lib/forms/clientes/pessoa_fisica/create.ex
@@ -0,0 +1,16 @@
+defmodule Forms.Clientes.Create do
+  alias Forms.{Error, PessoaFisica, Repo}
+
+  def call(params) do
+    params
+    |> PessoaFisica.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, _} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    Error.build(:bad_request, result)
+  end
+end
---
+++ lib/forms/clientes/pessoa_juridica/create.ex
@@ -0,0 +1,16 @@
+defmodule Forms.Clientes.PessoaJuridica.Create do
+  alias Forms.{Error, PessoaJuridica, Repo}
+
+  def call(params) do
+    params
+    |> PessoaJuridica.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, _} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    Error.build(:bad_request, result)
+  end
+end
