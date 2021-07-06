94636ad6446acb7855dd4be64da67f6c4c44478f
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-07-06 07:02:07 -0300

    controller created

---
+++ lib/forms_web/controllers/pessoa_juridica_controller.ex
@@ -0,0 +1,26 @@
+defmodule FormsWeb.PessoaJuridicaController do
+  use FormsWeb, :controller
+
+  alias Forms.PessoaJuridica
+  alias FormsWeb.ClienteView
+
+  action_fallback FormsWeb.FallbackController
+
+  def create(conn, params) do
+    with {:ok, %PessoaJuridica{} = cliente} <- Forms.create_pessoa_juridica(params) do
+      conn
+      |> put_status(:created)
+      |> put_view(ClienteView)
+      |> render("create.json", cliente: cliente)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, cliente} <- Forms.get_pessoa_juridica(id) do
+      conn
+      |> put_status(:ok)
+      |> put_view(ClienteView)
+      |> render("cliente.json", cliente: cliente)
+    end
+  end
+end
