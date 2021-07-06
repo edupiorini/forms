c929ec761d05b57baff582192e8614faf383cef2
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-07-06 07:02:28 -0300

    refactoring on pessoa fisica

--- lib/forms_web/controllers/pessoa_fisica_controller.ex
+++ lib/forms_web/controllers/pessoa_fisica_controller.ex
@@ -2,6 +2,7 @@ defmodule FormsWeb.PessoaFisicaController do
   use FormsWeb, :controller
 
   alias Forms.PessoaFisica
+  alias FormsWeb.ClienteView
 
   action_fallback FormsWeb.FallbackController
 
@@ -9,6 +10,7 @@ defmodule FormsWeb.PessoaFisicaController do
     with {:ok, %PessoaFisica{} = cliente} <- Forms.create_pessoa_fisica(params) do
       conn
       |> put_status(:created)
+      |> put_view(ClienteView)
       |> render("create.json", cliente: cliente)
     end
   end
@@ -17,7 +19,8 @@ defmodule FormsWeb.PessoaFisicaController do
     with {:ok, cliente} <- Forms.get_pessoa_fisica(id) do
       conn
       |> put_status(:ok)
-      |> render("pessoa_fisica.json", cliente: cliente)
+      |> put_view(ClienteView)
+      |> render("cliente.json", cliente: cliente)
     end
   end
 end
