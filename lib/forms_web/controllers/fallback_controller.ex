ab4d3751d13a2b75ea306b5c45ade90e1550095c
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-07-16 14:18:06 -0300

    fallback controller fixed

--- lib/forms_web/controllers/fallback_controller.ex
+++ lib/forms_web/controllers/fallback_controller.ex
@@ -10,4 +10,11 @@ defmodule FormsWeb.FallbackController do
     |> put_view(ErrorView)
     |> render("error.json", result: result)
   end
+
+  def call(conn, {:error, %Error{status: status, result: result}}) do
+    conn
+    |> put_status(status)
+    |> put_view(ErrorView)
+    |> render("error.json", result: result)
+  end
 end
