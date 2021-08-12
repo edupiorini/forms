646f7333b29dab48ddd151337827130bbc65177a
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-08-11 22:52:39 -0300

    new location for those tests

---
+++ test/forms/pessoa_fisica_test.exs
@@ -0,0 +1,43 @@
+defmodule FormsTest.PessoaFisica do
+  use ExUnit.Case
+
+  import FormsTest.Factory
+
+  alias Ecto.Changeset
+  alias Forms.PessoaFisica
+
+  describe "changeset/2" do
+    setup do
+      cliente = build(:pfisica_params)
+
+      {:ok, cliente: cliente}
+    end
+
+    test "when valid params are given, returns a changeset", %{cliente: cliente} do
+      response = PessoaFisica.changeset(cliente)
+
+      assert %Changeset{changes: %{nome: "Cliente de Teste"}, valid?: true} = response
+    end
+
+    test "when given new params, updates the changeset", %{cliente: cliente} do
+      updated_params = %{
+        "nome" => "Cliente Atualizado"
+      }
+
+      response =
+        cliente
+        |> PessoaFisica.changeset()
+        |> PessoaFisica.changeset(updated_params)
+
+      assert %Changeset{changes: %{nome: "Cliente Atualizado"}, valid?: true} = response
+    end
+
+    test "when invalid params are given, returns a changeset with an error" do
+      cliente = %{"nome" => "Cliente Errado"}
+
+      response = PessoaFisica.changeset(cliente)
+
+      assert %Changeset{changes: _changes, valid?: false} = response
+    end
+  end
+end
